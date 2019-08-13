ddaf80e98dcfabdbced441cf49b84a62c8d1a430
Vitor Santos <vitor.asantos@b2wdigital.com>
2019-08-13 14:27:59 -0300

    Added 100% CPU load; Made load permanent until process gets killed

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,38 @@
 #include <stdlib.h>
 #include <chrono>
 #include <thread>
+#include <iostream>
+#include <random>
 
 #define GIGABYTES 1 
 
 using namespace std;
 
 size_t gigabytes_as_bytes(size_t value) {
-	return value*1000000000;
+	return value*1024*1024*1024;
 }
 
 int main (void) {
+	random_device seeder;
+	mt19937 random_engine(seeder());
+	uniform_int_distribution<int8_t> generate_value(-10, 10);
 	size_t mem = gigabytes_as_bytes(GIGABYTES);
+	uniform_int_distribution<size_t> pick_index(0, mem);
+
+	cout << "Attempting to allocate " << GIGABYTES << " GB" << endl;
 
 	int8_t *vector_to_simulate_usage = new int8_t[mem];
 
-	this_thread::sleep_for(chrono::seconds(30));
+	cout << "Beginning CPU load, use CTRL+C to exit the process" << endl;
+	int8_t a, b;
+	size_t index;
+	while (true) {
+		index = pick_index(random_engine);
+		a = generate_value(random_engine);
+		b = generate_value(random_engine);
+		vector_to_simulate_usage[index] = a*b;
+		//this_thread::sleep_for(chrono::microseconds(5));
+	}
 	
 	delete [] vector_to_simulate_usage;
 
